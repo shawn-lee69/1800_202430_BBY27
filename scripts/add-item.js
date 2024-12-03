99d48b343dab940566d26a8ff5ca9e205c863459
Kyle Cheon <kcheon@my.bcit.ca>
2024-12-03 13:20:17 -0800

    fix: error

--- scripts/add-item.js
+++ scripts/add-item.js
@@ -195,12 +195,12 @@ function displaySearchResults(results) {
       resultsContainer.innerHTML = '';  // Clear search results
       resultsContainer.style.display = 'none'; // Hide the results container
 
-      // reset the additional item list to 'popular' items
-      favoritePopularBar.style.backgroundColor = '';
-      favoritePopularBar.style.color = '';
-      favoriteRecentBar.style.backgroundColor ='';
-      favoriteRecentBar.style.color = '';
-      renderPopularItems();
+      // // reset the additional item list to 'popular' items
+      // favoritePopularBar.style.backgroundColor = '';
+      // favoritePopularBar.style.color = '';
+      // favoriteRecentBar.style.backgroundColor ='';
+      // favoriteRecentBar.style.color = '';
+      // renderPopularItems();
     });
     resultsContainer.appendChild(resultItem); // Add the item to the results container
   });
